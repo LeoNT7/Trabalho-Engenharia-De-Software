df34e58ccd552dedc440db78e7eff710efb6bbc8
Leo Bento <lbento026@gmail.com>
2026-07-07 19:54:32 -0300

    Criando menu

---
+++ main.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+while True:
+    print("-----------------------------------------------")
+    print("------------ PRODUTOS NO ESTOQUE --------------")
+    print("-----------------------------------------------")
+    print(" 1 - Registrar entrada de produto")
+    print(" 2 - Retirar produto")
+    print(" 3 - Sair")
+    print("-----------------------------------------------")
+    op = input("Digite a opção >>")
+    
+
+
+    
+
+
+
+    
