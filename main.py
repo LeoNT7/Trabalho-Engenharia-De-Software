4c84bad47511722d1c4fd86b0a186a82fc947a8a
Leo Bento <lbento026@gmail.com>
2026-07-07 21:05:57 -0300

    Finalizando menu de opções de registrar entrada de produto e retirar produto

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-
-
-
-
+produtos = {'item1': 2, 'item2': 0, 'item3': 5}
 
 while True:
     print("-----------------------------------------------")
-    print("------------ PRODUTOS NO ESTOQUE --------------")
+    print("------- SISTEMA DE PRODUTOS NO ESTOQUE --------")
     print("-----------------------------------------------")
     print(" 1 - Registrar entrada de produto")
     print(" 2 - Retirar produto")
@@ -20,15 +17,37 @@ while True:
             print("0 - Voltar")
             op = int(input("Digite a opção >>"))
             if op == 0:
+                print("-----------------------------------------------")
+                print()
+                print()
+                print("Voltando...")
+                print()
+                print()
+                print("-----------------------------------------------")
                 break
             
     elif menuOp == 2:
         while True:
-            print("------------ REGISTRAR RETIRADA DE PRODUTOS ------------")
-            print("--------------------------------------------------------")
+            print("------- REGISTRAR RETIRADA DE PRODUTOS --------")
+            print("-----------------------------------------------")
+            print("CODIGO | PRODUTO  | QUANTIDADE                 ")
+                   
+            i = 0
+            for produto, quantidade in produtos.items():
+                print(f"  {i}       {produto}         {quantidade}     ")
+                i += 1
+            print("Lista contém", i, "itens")
+
             print("0 - Voltar")
-            op = int(input("Digite a opção >>"))
+            op = int(input("Digite o produto a ser retirado >>"))
             if op == 0:
+                print("-----------------------------------------------")
+                print()
+                print()
+                print("Voltando...")
+                print()
+                print()
+                print("-----------------------------------------------")
                 break
 
     elif menuOp == 3:
